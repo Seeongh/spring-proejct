4821c0a1694cdae116a8fb5a530715e3eb775745
안성현 <mikstic7@gmail.com>
2024-04-30 23:57:31 +0900

    <repeat> servlet - 복습하면서 헷갈리는 부분 개념 다시잡기: Spring boot 내장톰캣이 서블릿의 Service에 request, response객체를 전달

--- SpringCore/sevlet/src/main/java/hello/sevlet/basic/HelloServlet.java
+++ SpringCore/sevlet/src/main/java/hello/sevlet/basic/HelloServlet.java
@@ -7,8 +7,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * @WebServlet으로 서블릿 애노테이션을 선언
+ * name : 서블릿 이름, urlPattern: url매핑
+ */
 @WebServlet(name="helloServlet", urlPatterns="/hello")
 public class HelloServlet extends HttpServlet {
+    /**
+     * HTTP요청을 통해 매핑된 URL 이 호출되면 서블릿 컨테이너는 service 메서드를 실행
+     * spring boot 실행하면서 내장 톰캣이 띄워짐
+     * 톰캣은 서블릿컨테이너를 통해 서블릿을 생성함(지금 이 이름이 있는 서블릿)
+     * 브라우저가 HTTP 메세지를 생성해서 던지면
+     * 서버는 그걸받고 response, request 객체를 만들어서 여기 service에 전달.
+     */
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("HelloServlet.service");
