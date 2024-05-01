070d7649623bdd9b1c7a6547a6b39233f9bf0cfa
안성현 <mikstic7@gmail.com>
2024-05-01 23:43:01 +0900

    <review> servlet - 서블릿 컨테이너, 서블릿 동작 원리 복습

--- SpringCore/sevlet/src/main/java/hello/sevlet/SevletApplication.java
+++ SpringCore/sevlet/src/main/java/hello/sevlet/SevletApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+/**
+ * [기존] 서블릿 코드를 클래스 파일로 빌드해서 톰캣같은 웹서버에 배포
+ * [Spring Boot] 톰캣 서버를 내장하고 있어, 톰캣 서버 설치 없이 편리하게 실행
+ * @ServletComponentScan 을 통해 서블릿을 직접 등록해서 사용함.
+ */
 @ServletComponentScan //서블릿 자동등록
 @SpringBootApplication
 public class SevletApplication {
--- SpringCore/sevlet/src/main/java/hello/sevlet/basic/HelloServlet.java
+++ SpringCore/sevlet/src/main/java/hello/sevlet/basic/HelloServlet.java
@@ -10,15 +10,19 @@ import java.io.IOException;
 /**
  * @WebServlet으로 서블릿 애노테이션을 선언
  * name : 서블릿 이름, urlPattern: url매핑
+ *
+ * [서블릿내부 동작]
+ * 와스(톰캣) 안에 서블릿 컨테이너가 자동으로 서블릿 객체를 생성하고 , 호출해줌(와스종료시 서블릿도 종료해줌 -생명주기 관리)
+ * url 로 요청이 들어오면 요청메세지 기반으로Request,Response객체 만들어서 서블릿 객체 호출
+ *  (- request, response는 매번 새로 만드는게 맞지만 , servlet은 항상 새로 생성할필요가없어 싱글톤으로 재사용됨)
+ *  (- was는 동시 요청을 위한 멀티 쓰레드 처리 지원)
+ * hello 서비스 실행
+ * response 객체를 보고 응답
  */
 @WebServlet(name="helloServlet", urlPatterns="/hello")
 public class HelloServlet extends HttpServlet {
     /**
      * HTTP요청을 통해 매핑된 URL 이 호출되면 서블릿 컨테이너는 service 메서드를 실행
-     * spring boot 실행하면서 내장 톰캣이 띄워짐
-     * 톰캣은 서블릿컨테이너를 통해 서블릿을 생성함(지금 이 이름이 있는 서블릿)
-     * 브라우저가 HTTP 메세지를 생성해서 던지면
-     * 서버는 그걸받고 response, request 객체를 만들어서 여기 service에 전달.
      */
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
